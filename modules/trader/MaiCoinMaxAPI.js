2f2db5c7531a0221b1373bc08c96e4fdec479f09
ColorfulCakeChen <colorfulcakechen@gmail.com>
2020-04-09 15:09:50 +0800

    Combined into keys.

--- modules/trader/MaiCoinMaxAPI.js
+++ modules/trader/MaiCoinMaxAPI.js
@@ -2,6 +2,8 @@ var crypto = require("modules/crypto/sjcl.js"); // Stanford Javascript Crypto Li
 var defaultStoreDocuments = require("document");
 var http = require("http");
 
+var METHOD_GET = "GET";
+
 var API_ROOT = "https://max-api.maicoin.com";
 
 var API_PATH_TICKERS =          "/api/v2/tickers";
@@ -41,11 +43,11 @@ function A_S_Key_get_FromStore() {
 /** Issue http request with digested body.
  * @param {string} method     The http request method. e.g. "GET"
  * @param {Object} body       The http request body.
- * @param {string} secret_key The key to digest the body.
- * @param {string} access_key The value used in http request header X-MAX-ACCESSKEY.
+ * @param {string} keys.s_key The key (secret_key) to digest the body.
+ * @param {string} keys.a_key The access_key used in http request header X-MAX-ACCESSKEY.
  * @return {Object} The response.body, if success. Return null, otherwise.
  */
-function HMAC_SHA256_HTTP_Request( method, body, secret_key, access_key ) {
+function HMAC_SHA256_HTTP_Request( method, body, keys ) {
 
     var requestObject = {
         url: API_ROOT + body.path,
@@ -56,58 +58,35 @@ function HMAC_SHA256_HTTP_Request( method, body, secret_key, access_key ) {
     };
 
     // If there are keys, sign it.
-    if (secret_key && access_key) {
-        var payload = btoa(JSON.stringify(body)); // to Base64.
+    if (keys && keys.s_key && keys.a_key) {
+        var payload = btoa(requestObject.bodyString); // to Base64.
+        //return payload;
 
-        var hmac = new crypto.sjcl.misc.hmac(secret_key, crypto.sjcl.hash.sha256);
+        var hmac = new crypto.sjcl.misc.hmac(keys.s_key, crypto.sjcl.hash.sha256);
         hmac.update(payload);
         var digestNumberArray = hmac.digest();
-    //    return digestNumberArray;
+        //return digestNumberArray;
 
         // Convert number array to unsigned integer, and then convert to HEX, and then concate to one string.
         var signature = (digestNumberArray.map(function (x) { return (x>>>0).toString(16); })).join("");
-    //    return signature;
+        //return signature;
 
     	requestObject.headers = {
-            'X-MAX-ACCESSKEY': access_key,
+            'X-MAX-ACCESSKEY': keys.a_key,
             'X-MAX-PAYLOAD': payload,
             'X-MAX-SIGNATURE': signature
         };
     }
 
     var response = http.request(requestObject);
-    return response;
+    //return response;
+    return response.body;
+/*
     if (response.status !== "200")
         return;
 
     var responseBody = JSON.parse(response.body);
     return responseBody;
-/*
-    try {
-        var hmacKey = "1234";
-        var hmac = new crypto.sjcl.misc.hmac(hmacKey, crypto.sjcl.hash.sha256);
-        hmac.update("Hello");
-        var t = hmac.digest();
-        return t;
-    } catch (e) {
-        return e;
-    }
-*/
-/*
-    let payload = new Buffer(JSON.stringify(body)).toString('base64');
-    let signature = crypto.createHmac('sha256', secret_key).update(payload).digest('hex');
-    let options = {
-      method: 'GET',
-    //  headers: {
-    //    'X-MAX-ACCESSKEY': access_key,
-    //    'X-MAX-PAYLOAD': payload,
-    //    'X-MAX-SIGNATURE': signature
-      },
-      uri: apiPath + bodyGetTickersBTCTWD.path,
-      json: true,
-      body: bodyGetTickersBTCTWD
-    };
-    request(options, function(error, response, body) { ... });
 */
 }
 
@@ -115,32 +94,12 @@ function HMAC_SHA256_HTTP_Request( method, body, secret_key, access_key ) {
  * @param path_market e.g. "btctwd"
  */
 function tickers_PathMarket_get( path_market ) {
-	var method = "GET";
-
     var body = {
       path: API_PATH_TICKERS + "/" + path_market,
       nonce: Date.now()
     };
-
-    var responseBody = HMAC_SHA256_HTTP_Request( method, body );
+    var responseBody = HMAC_SHA256_HTTP_Request( METHOD_GET, body );
     return responseBody;
-/*    
-    var requestObject = {
-        url: API_ROOT + body.path,
-    //    "params" : {
-    //    },
-        method: "GET",
-        bodyString: JSON.stringify(body)
-    };
-
-    var response = http.request(requestObject);
-    //return response;
-    if (response.status !== "200")
-        return;
-
-    var responseBody = JSON.parse(response.body);
-    return responseBody;
-*/
 }
 
 /** Get the current state of the BTCTWD market.
@@ -150,12 +109,22 @@ function tickers_BTCTWD_get() {
     return tickers_PathMarket_get( path_market );
 }
 
+/** Get the current balance of all currency.
+ */
+function membersAccounts_get() {
+    var body = {
+      path: API_PATH_MEMBERS_ACCOUNTS,
+      nonce: Date.now()
+    };
+    var keys = A_S_Key_get_FromStore();
+    var responseBody = HMAC_SHA256_HTTP_Request( METHOD_GET, body, keys );
+    return responseBody;
+}
+
 /** Get the current balance of the specified currency.
  * @param path_currency e.g. "twd", or "btc"
  */
-function membersAccountsCurrency_PathCurrency_get( path_currency ) {
-	var method = "GET";
-
+function membersAccounts_PathCurrency_get( path_currency ) {
     var body = {
       path: API_PATH_MEMBERS_ACCOUNTS + "/" + path_currency,
       nonce: Date.now()
@@ -165,12 +134,12 @@ function membersAccountsCurrency_PathCurrency_get( path_currency ) {
     //var access_key = "5678";
     var keys = A_S_Key_get_FromStore();
 
-    var responseBody = HMAC_SHA256_HTTP_Request( method, body, keys.s_key, keys.a_key );
+    var responseBody = HMAC_SHA256_HTTP_Request( METHOD_GET, body, keys );
     return responseBody;
 }
 
 /** Get the current balance of the TWD currency.
  */
-function membersAccountsCurrency_TWD_get() {
-    return membersAccountsCurrency_PathCurrency_get( "twd" );
+function membersAccounts_TWD_get() {
+    return membersAccounts_PathCurrency_get( "twd" );
 }
